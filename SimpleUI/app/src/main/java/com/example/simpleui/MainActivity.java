603294b923fca56a0158fc46c8c3e17c993ded52
godgunman <godgunman@gmail.com>
2015-10-08 20:39:23 +0800

    Implement click method of submit button

--- SimpleUI/app/src/main/java/com/example/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/simpleui/MainActivity.java
@@ -4,7 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +18,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         inputText = (EditText) findViewById(R.id.inputText);
-        inputText.setText("aaa");
+    }
+
+    public void submit(View view) {
+
+        String text = inputText.getText().toString();
+        Toast.makeText(this, text, Toast.LENGTH_LONG).show();
     }
 
     @Override
