0b7c824e7105d7a5b644769d4d672fe47da443a3
ggm <ggm@skymizer.com>
2015-10-29 20:40:27 +0800

    show StaticMap image on WebView and ImageView

--- SimpleUI/app/src/main/java/com/example/simpleui/OrderDetailActivity.java
+++ SimpleUI/app/src/main/java/com/example/simpleui/OrderDetailActivity.java
@@ -1,21 +1,30 @@
 package com.example.simpleui;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.webkit.WebView;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class OrderDetailActivity extends AppCompatActivity {
 
     private TextView addressTextView;
+    private WebView webView;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_detail);
 
+        webView = (WebView) findViewById(R.id.webView);
+
         addressTextView = (TextView) findViewById(R.id.address);
+        imageView = (ImageView) findViewById(R.id.imageView);
 
         String storeInfo = getIntent().getStringExtra("store_info");
         final String address = storeInfo.split(",")[1];
@@ -41,8 +50,29 @@ public class OrderDetailActivity extends AppCompatActivity {
         }
 
         @Override
-        protected void onPostExecute(String result) {
-            addressTextView.setText(result);
+        protected void onPostExecute(String latLng) {
+            addressTextView.setText(latLng);
+
+            String staticMapUrl = Utils.getStaticMapUrl(latLng, "17", "300x600");
+            webView.loadUrl(staticMapUrl);
+
+            StaticMapTask task = new StaticMapTask();
+            task.execute(latLng);
+        }
+    }
+
+    private class StaticMapTask extends AsyncTask<String, Integer, byte[]> {
+
+        @Override
+        protected byte[] doInBackground(String... params) {
+            String staticMapUrl = Utils.getStaticMapUrl(params[0], "17", "300x600");
+            return Utils.urlToBytes(staticMapUrl);
+        }
+
+        @Override
+        protected void onPostExecute(byte[] bytes) {
+            Bitmap bm = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+            imageView.setImageBitmap(bm);
         }
     }
 }
