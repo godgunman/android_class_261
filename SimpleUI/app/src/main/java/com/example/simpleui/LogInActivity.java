505a76ea0dc7d8e0cce7673f783fc143c622b5e4
tony056 <tony61507@gmail.com>
2015-11-05 21:34:18 +0800

    Added newMeRequest and go to main activity

--- SimpleUI/app/src/main/java/com/example/simpleui/LogInActivity.java
+++ SimpleUI/app/src/main/java/com/example/simpleui/LogInActivity.java
@@ -6,14 +6,21 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class LogInActivity extends AppCompatActivity {
     CallbackManager callbackManager;
@@ -24,9 +31,32 @@ public class LogInActivity extends AppCompatActivity {
         setContentView(R.layout.activity_log_in);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        if(accessToken != null){
+            GraphRequest.newMeRequest(accessToken, new GraphRequest.GraphJSONObjectCallback() {
+                @Override
+                public void onCompleted(JSONObject object, GraphResponse response) {
+                    String username = null;
+                    try {
 
+                        username = object.getString("name");
+                        Log.d("Login", username);
+                        goToMainActivity(username);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }).executeAsync();
+        }
         callbackManager = CallbackManager.Factory.create();
         setUpFacebookLogin();
+    }
+
+    private void goToMainActivity(String username) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("username", username);
+        startActivity(intent);
     }
 
     private void setUpFacebookLogin() {
